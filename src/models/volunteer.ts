debc0b96236b62ecfef5785d2613d5cdaf4c4012
Brendon Jensen <94388382+BigFoot-adventures@users.noreply.github.com>
2023-07-21 13:54:00 -0400

    first draft volunteer model

---
+++ src/models/volunteer.ts
@@ -0,0 +1,45 @@
+export class volunteer {
+
+    first: string;
+    last: string;
+    userName: string;
+    password: string;
+    preferedLocations: string[] = [];
+    skills_Interests: string[] = [];
+    availability: string[] = [];
+    address: string;
+    phone: string;
+    email: string;
+    education: string;
+    currentLicenses: string[] = [];
+    emergencyContact: string;
+    emergencyPhone: string;
+    emergencyEmail: string;
+    emergencyAddress: string;
+    driversLicense: boolean;
+    SSCard: boolean;
+    approved: boolean;
+
+    constructor(first: string, last: string, userName: string, password: string, preferedLocations: string[], skills_Interests: string[], availability: string[], address: string, phone: string, email: string, education: string, currentLicenses: string[], emergencyContact: string, emergencyPhone: string, emergencyEmail: string, emergencyAddress: string, driversLicense: boolean, SSCard: boolean, approved: boolean) {
+        this.first = first;
+        this.last = last;
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.preferedLocations = preferedLocations;
+        this.skills_Interests = skills_Interests;
+        this.availability = availability;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.education = education;
+        this.currentLicenses = currentLicenses;
+        this.emergencyContact = emergencyContact;
+        this.emergencyPhone = emergencyPhone;
+        this.emergencyEmail = emergencyEmail;
+        this.emergencyAddress = emergencyAddress;
+        this.driversLicense = driversLicense;
+        this.SSCard = SSCard;
+        this.approved = approved;
+    }
+}
