bcb744f7d89408ce1179e62fc6ca0d37b46c0651
Brendon Jensen <94388382+BigFoot-adventures@users.noreply.github.com>
2023-07-22 11:24:33 -0400

    added swagger api documentation

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,27 @@
 import express from 'express';
 import cors from 'cors';
 import { userRouter } from './routers/userRouter';
+import swaggerUI from 'swagger-ui-express';
+import { readFileSync } from 'fs';
+import path from 'path';
 
 let port = 3000;
-
+let swagger_doc = readFileSync(path.join(process.cwd(),'src','openapi.json'));
+let swagger_obj = JSON.parse(swagger_doc.toString());
 let app = express();
+
 app.use(cors({
     origin: '*',
     credentials: true
 }));
+
 app.use(express.json());
 
 app.use('/users', userRouter);
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use('/', swaggerUI.serve, swaggerUI.setup(swagger_obj));
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => { console.log(`Server is running on port ${port}`); });
 
 /* from here we, set up endpoints for our application
     - an endpoint looks like this: http://localhost:3000/api/endpoint
