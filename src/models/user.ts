da94b6abb01dfdbdf5fba6ea16ef5e3f822fdbc5
Brendon Jensen <94388382+BigFoot-adventures@users.noreply.github.com>
2023-08-10 23:55:16 -0400

    added validating method

--- src/models/user.ts
+++ src/models/user.ts
@@ -22,7 +22,7 @@ export class user {
     approved: boolean;
 
     constructor(role: 'volunteer' | 'admin', first: string, last: string, userName: string, password: string, preferedLocations: string[], skills_Interests: string[], availability: string[], address: string, phone: string, email: string, education: string, currentLicenses: string[], emergencyContact: string, emergencyPhone: string, emergencyEmail: string, emergencyAddress: string, driversLicense: boolean, SSCard: boolean, approved: boolean) {
-        this. role = role;
+        this.role = role;
         this.first = first;
         this.last = last;
         this.userName = userName;
@@ -68,5 +68,50 @@ export class user {
             approved: this.approved
         }
         return result;
+    }
+
+    isValid() {
+        let valid = true;
+        if(this.role != 'volunteer' && this.role != 'admin')
+            valid = false;
+        else if(typeof this.first != 'string' || this.first.length == 0)
+            valid = false;
+        else if(typeof this.last != 'string' || this.last.length == 0)
+            valid = false;
+        else if(typeof this.userName != 'string' || this.userName.length == 0)
+            valid = false;
+        else if(typeof this.password != 'string' || this.password.length == 0)
+            valid = false;
+        else if(typeof this.preferedLocations != 'object' || this.preferedLocations.length == 0)
+            valid = false;
+        else if(typeof this.skills_Interests != 'object' || this.skills_Interests.length == 0)
+            valid = false;
+        else if(typeof this.availability != 'object' || this.availability.length == 0)
+            valid = false;
+        else if(typeof this.address != 'string' || this.address.length == 0)
+            valid = false;
+        else if(typeof this.phone != 'string' || this.phone.length == 0)
+            valid = false;
+        else if(typeof this.email != 'string' || this.email.length == 0)
+            valid = false;
+        else if(typeof this.education != 'string' || this.education.length == 0)
+            valid = false;
+        else if(typeof this.currentLicenses != 'object' || this.currentLicenses.length == 0)
+            valid = false;
+        else if(typeof this.emergencyContact != 'string' || this.emergencyContact.length == 0)
+            valid = false;
+        else if(typeof this.emergencyPhone != 'string' || this.emergencyPhone.length == 0)
+            valid = false;
+        else if(typeof this.emergencyEmail != 'string' || this.emergencyEmail.length == 0)
+            valid = false;
+        else if(typeof this.emergencyAddress != 'string' || this.emergencyAddress.length == 0)
+            valid = false;
+        else if(typeof this.driversLicense != 'boolean')
+            valid = false;
+        else if(typeof this.SSCard != 'boolean')
+            valid = false;
+        else if(typeof this.approved != 'boolean')
+            valid = false;
+        return valid;
     }
 }
